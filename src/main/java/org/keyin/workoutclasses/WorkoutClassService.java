93b4556a80690d790bab2f0f650a63f08e847274
Jordan Kelloway <106637327+JKells99@users.noreply.github.com>
2025-03-24 09:23:56 -0230

    Update WorkoutClassService.java

--- src/main/java/org/keyin/workoutclasses/WorkoutClassService.java
+++ src/main/java/org/keyin/workoutclasses/WorkoutClassService.java
@@ -6,8 +6,4 @@ public class WorkoutClassService {
     // Inject in the DAO to be able to use it in the service
     private WorkoutClassDAO workoutDAO;
 
-    public void addNewWorkoutClass(WorkoutClass workoutClass) throws SQLException {
-        System.out.println("Adding new workout class");
-        workoutDAO.addWorkoutClass(workoutClass);
-    }
 }
