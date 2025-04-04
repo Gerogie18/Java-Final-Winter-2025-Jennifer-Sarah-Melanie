1ed3c0973a6400c7bf6f88193261c69de20f5b97
Sarah Kristian Perry <sarah.perry@keyin.com>
2025-04-03 23:13:05 -0230

    Add create table scripts for users, workoutclasses, and memberships.

--- src/main/resources/scripts.sql
+++ src/main/resources/scripts.sql
@@ -1,43 +1,48 @@
 CREATE TABLE users (
-                       user_id SERIAL PRIMARY KEY,
-                       username VARCHAR(50) NOT NULL,
-                       email VARCHAR(100) NOT NULL UNIQUE,
-                       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+   user_id SERIAL PRIMARY KEY,
+   user_name VARCHAR(50) NOT NULL,
+   user_email VARCHAR(100) NOT NULL UNIQUE,
+   user_role VARCHAR(10) NOT NULL CHECK(role IN('ADMIN','TRAINER','MEMBER')) CONSTRAINT valid_role_check
+   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE cars (
-                      car_id SERIAL PRIMARY KEY,
-                      make VARCHAR(50) NOT NULL,
-                      model VARCHAR(50) NOT NULL,
-                      year INT NOT NULL,
-                      price DECIMAL(10, 2) NOT NULL,
-                      seller_id INT NOT NULL,
-                      FOREIGN KEY (seller_id) REFERENCES users(user_id) ON DELETE CASCADE
+CREATE TABLE workout_classes (
+    class_id SERIAL PRIMARY KEY,
+    class_name VARCHAR(50) NOT NULL,
+    class_type VARCHAR(50) NOT NULL,
+    class_description TEXT NOT NULL,
+    class_status VARCHAR(20) NOT NULL CHECK(status IN ('ACTIVE','CANCELLED','INACTIVE')) CONSTRAINT status_check
+    class_capacity INT CHECK (class_capacity >= 0 AND class_capacity <= 100),
+    trainer_id INT NOT NULL,
+    CONSTRAINT fk_workout_classes_trainer_id
+        FOREIGN KEY (trainer_id)
+        REFERENCES users(user_id)
+        ON UPDATE NO ACTION
+        ON DELETE CASCADE
 );
 
 
 
-CREATE TABLE IF NOT EXISTS public.space_fleet_memberships
-(
+CREATE TABLE IF NOT EXISTS memberships (
     membership_id SERIAL PRIMARY KEY,
-    membership_tier VARCHAR(50) NOT NULL,
-    membership_credits INTEGER NOT NULL,
-    membership_log TEXT,
-    date_registered DATE DEFAULT CURRENT_DATE,
-    astronaut_id INTEGER NOT NULL,
-    CONSTRAINT space_fleet_memberships_astronaut_fkey FOREIGN KEY (astronaut_id)
-    REFERENCES public.astronauts (astronaut_id)
-    ON UPDATE NO ACTION
-    ON DELETE CASCADE
-    );
+    membership_type VARCHAR(50) NOT NULL,
+    membership_cost DECIMAL(10, 2) NOT NULL,
+    membership_description TEXT,
+    date_purchased DATE DEFAULT CURRENT_DATE,
+    member_id INT NOT NULL,
+    CONSTRAINT fk_memberships_member_id
+        FOREIGN KEY (member_id) REFERENCES users(user_id)
+        ON UPDATE NO ACTION
+        ON DELETE CASCADE
+);
 
 TABLESPACE pg_default;
 
 
--- Getting sum for sumberships by month example
+-- Getting sum for memberships by month example
 SELECT
     TO_CHAR(date_purchased, 'YYYY-MM') AS month,
     SUM(membership_price) AS total_revenue
-FROM public.memberships
+FROM memberships
 GROUP BY month
 ORDER BY month;
