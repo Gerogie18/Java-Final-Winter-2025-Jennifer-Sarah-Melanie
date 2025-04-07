d09fefb2bf6545706a09350b64634caf3716b233
Gerogie18 <jennlyver@gmail.com>
2025-04-07 03:54:23 -0230

    user no longer abstract

--- src/main/java/org/keyin/user/User.java
+++ src/main/java/org/keyin/user/User.java
@@ -6,7 +6,7 @@ import org.mindrot.jbcrypt.BCrypt;
 // This is the parent class for all users, There are 3 types of users: Trainer, Member, and Admin
 //
 // *//
-public abstract class User {
+public class User {
     private static int UserId = 1;
 
     private final String userId;
