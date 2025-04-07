754adfb18259510600d52116312693870f759523
Gerogie18 <jennlyver@gmail.com>
2025-04-07 03:52:39 -0230

    Revert "Update User.java"
    This reverts commit 48e5b75db886d8f394a8598926934a9a7ed0acaa.

--- src/main/java/org/keyin/user/User.java
+++ src/main/java/org/keyin/user/User.java
@@ -6,7 +6,7 @@ import org.mindrot.jbcrypt.BCrypt;
 // This is the parent class for all users, There are 3 types of users: Trainer, Member, and Admin
 //
 // *//
-public class User {
+public abstract class User {
     private static int UserId = 1;
 
     private final String userId;
