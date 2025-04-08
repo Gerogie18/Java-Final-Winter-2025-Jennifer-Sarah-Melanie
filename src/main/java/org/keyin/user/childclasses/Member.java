04ca4ee4ba833ae66f6ebcca8f62f050069bf4da
Gerogie18 <jennlyver@gmail.com>
2025-04-08 18:20:58 -0230

    Update child classes

--- src/main/java/org/keyin/user/childclasses/Member.java
+++ src/main/java/org/keyin/user/childclasses/Member.java
@@ -3,4 +3,7 @@ package org.keyin.user.childclasses;
 import org.keyin.user.User;
 
 public class Member extends User {
+    public Member (String username, String password, String email, int phoneNumber, String address) {
+        super(username, password, email, phoneNumber, address, Role.MEMBER);
+    }
 }
--- src/main/java/org/keyin/user/childclasses/Trainer.java
+++ src/main/java/org/keyin/user/childclasses/Trainer.java
@@ -3,4 +3,7 @@ package org.keyin.user.childclasses;
 import org.keyin.user.User;
 
 public class Trainer extends User {
+    public Trainer (String username, String password, String email, int phoneNumber, String address) {
+        super(username, password, email, phoneNumber, address, Role.TRAINER);
+    }
 }
