190cfb256036720013a727a889834aaef9b4fc1e
Sarah Kristian Perry <sarah.perry@keyin.com>
2025-04-08 11:25:45 -0230

    Correcting create table sqls for db

--- src/main/resources/scripts.sql
+++ src/main/resources/scripts.sql
@@ -1,9 +1,11 @@
 CREATE TABLE users (
    user_id SERIAL PRIMARY KEY,
    user_name VARCHAR(50) NOT NULL,
+   user_phone_number VARCHAR(15) NOT NULL,
    user_email VARCHAR(100) NOT NULL UNIQUE,
-   user_role VARCHAR(10) NOT NULL CHECK(role IN('ADMIN','TRAINER','MEMBER')) CONSTRAINT valid_role_check
-   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+   user_address VARCHAR(100) NOT NULL,
+   user_role VARCHAR(10) NOT NULL,
+   CONSTRAINT valid_role_check CHECK (user_role IN ('ADMIN', 'TRAINER', 'MEMBER'))
 );
 
 CREATE TABLE workout_classes (
@@ -11,14 +13,18 @@ CREATE TABLE workout_classes (
     class_name VARCHAR(50) NOT NULL,
     class_type VARCHAR(50) NOT NULL,
     class_description TEXT NOT NULL,
-    class_status VARCHAR(20) NOT NULL CHECK(status IN ('ACTIVE','CANCELLED','INACTIVE')) CONSTRAINT status_check
-    class_capacity INT CHECK (class_capacity >= 0 AND class_capacity <= 100),
+    class_status VARCHAR(20) NOT NULL,
+    class_capacity INT,
     trainer_id INT NOT NULL,
     CONSTRAINT fk_workout_classes_trainer_id
         FOREIGN KEY (trainer_id)
         REFERENCES users(user_id)
         ON UPDATE NO ACTION
-        ON DELETE CASCADE
+        ON DELETE CASCADE,
+    CONSTRAINT status_check
+        CHECK(class_status IN ('ACTIVE','CANCELLED','INACTIVE')),
+    CONSTRAINT capacity_check
+        CHECK (class_capacity >= 0 AND class_capacity <= 100)
 );
 
 
