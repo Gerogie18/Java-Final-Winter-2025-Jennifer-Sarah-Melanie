e6846baab0644d579a6e921a821396ae15571944
melanie <114537207+todayisthorsday@users.noreply.github.com>
2025-04-15 11:38:21 -0230

    Added back in the membership class after it was lost during merge

--- src/main/java/GymApp/models/Membership.java
+++ src/main/java/GymApp/models/Membership.java
@@ -4,5 +4,82 @@ package GymApp.models;
 // This is class file that represents a membership
 //
 public class Membership {
+    private int membershipID;
 
+    private String membershipType;
+
+    private String membershipDescription;
+
+    private double membershipCost;
+
+    private int memberID;
+
+    public Membership(int membershipID, String membershipType, String membershipDescription, double membershipCost,
+            int memberID) {
+        this.membershipID = membershipID;
+        this.membershipType = membershipType;
+        this.membershipDescription = membershipDescription;
+        this.membershipCost = membershipCost;
+        this.memberID = memberID;
+    }
+
+    public Membership(String membershipType, String membershipDescription, double membershipCost,
+            int memberID) {
+        this.membershipType = membershipType;
+        this.membershipDescription = membershipDescription;
+        this.membershipCost = membershipCost;
+        this.memberID = memberID;
+    }
+
+    // Getters and Setters
+    public int getMembershipID() {
+        return membershipID;
+    }
+
+    public void setMembershipID(int membershipID) {
+        this.membershipID = membershipID;
+    }
+
+    public String getMembershipType() {
+        return membershipType;
+    }
+
+    public void setMembershipType(String membershipType) {
+        this.membershipType = membershipType;
+    }
+
+    public String getMembershipDescription() {
+        return membershipDescription;
+    }
+
+    public void setMembershipDescription(String membershipDescription) {
+        this.membershipDescription = membershipDescription;
+    }
+
+    public double getMembershipCost() {
+        return membershipCost;
+    }
+
+    public void setMembershipCost(double membershipCost) {
+        this.membershipCost = membershipCost;
+    }
+
+    public int getMemberID() {
+        return memberID;
+    }
+
+    public void setMemberID(int memberID) {
+        this.memberID = memberID;
+    }
+
+    @Override
+    public String toString() {
+        return "Membership{" +
+                "membershipID=" + membershipID +
+                ", membershipType='" + membershipType + '\'' +
+                ", membershipDescription='" + membershipDescription + '\'' +
+                ", membershipCost=" + membershipCost +
+                ", memberID=" + memberID +
+                '}';
+    }
 }
